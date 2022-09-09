54ec12791354bba15bf90b77d4aa8651644e9ba9
Renzo Gorosito <renzog6@gmail.com>
2022-09-09 09:47:13 -0300

    Inicio

---
+++ src/main/java/ar/nex/neumatico/ServiceNeumaticoApplication.java
@@ -0,0 +1,13 @@
+package ar.nex.neumatico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceNeumaticoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceNeumaticoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/nex/sae/neumatico/ServiceNeumaticoApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.nex.sae.neumatico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceNeumaticoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
